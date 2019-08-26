62134091f2eb3b9adf0a270902700dd6232abf42
John Young <jfy@uw.edu>
2019-08-26 09:04:10 -0700

    Refactors express app out of the server

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import path from "path";
+import { dbg } from "./util/tools";
+
+require("dotenv").config();
+
+// Get an Express app
+const app = express();
+
+// logging
+app.use(morgan("dev"));
+
+// Body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//connect to database
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// include the routes
+import routes from "./routes";
+app.use(routes);
+
+// Serve static assets if in production and not found
+if (process.env.NODE_ENV === "production") {
+  //Set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  });
+}
+
+export default app;
--- server.js
+++ server.js
@@ -1,28 +1,7 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import path from "path";
+import app from "./app";
 
-// Get an Express app
-const app = express();
+// Set port to listen to
 const PORT = process.env.PORT || 3001;
 
-// logging
-app.use(morgan("dev"));
-
-// Public static routes
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-// Body parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
-app.options("*", cors());
-
-import routes from "./routes";
-app.use(routes);
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+// Start listening
+export default app.listen(PORT);
