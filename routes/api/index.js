6ecebdcc91aff872da1229b6d99fb01bebec9b7c
John Young <john@codeandcreative.com>
2020-03-24 10:16:35 -0700

    Removes some downstream code additions

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,6 @@ import authRoutes from "./auth";
 import installRoutes from "./install";
 import usersRoutes from "./users";
 import eventsRoutes from "./events";
-import statsRoutes from "./stats";
 
 router.get("/", (req, res) => {
   res.send("Hello from /api/");
@@ -15,6 +14,5 @@ router.use("/auth", authRoutes);
 router.use("/install", installRoutes);
 router.use("/users", usersRoutes);
 router.use("/events", eventsRoutes);
-router.use("/stats", statsRoutes);
 
 export default router;
