f26f4004908c279a2303ff094758416107cc5d81
John Young <john@codeandcreative.com>
2020-03-23 21:23:50 -0700

    Adds a logError function

--- util/tools/logging.js
+++ util/tools/logging.js
@@ -3,27 +3,47 @@ import "winston-mongodb";
 require("dotenv").config();
 
 const reqestDeniedLogger = createLogger({
-    format: format.json(),
-    transports: [
-        new transports.Console(),
-        new transports.MongoDB({
-            level: "info",
-            db: process.env.MONGODB_URI,
-            collection: "deniedRequests"
-        })
-    ]
+  format: format.json(),
+  transports: [
+    new transports.Console(),
+    new transports.MongoDB({
+      level: "info",
+      db: process.env.MONGODB_URI,
+      collection: "deniedRequests"
+    })
+  ]
 });
 
-export const logDeniedRequest = async (data) => {
-    const result = await reqestDeniedLogger.info(data.req.useragent.source, {
-        metadata: {
-            device: data.req.device ? data.req.device._id : null,
-            time: new Date(),
-            method: data.req.method,
-            ip: data.req.ip,
-            hostname: data.req.hostname,
-            path: data.req.path,
-            xhr: data.req.xhr
-        }
-    });
+export const logDeniedRequest = async data => {
+  const result = await reqestDeniedLogger.info(data.req.useragent.source, {
+    metadata: {
+      device: data.req.device ? data.req.device._id : null,
+      time: new Date(),
+      method: data.req.method,
+      ip: data.req.ip,
+      hostname: data.req.hostname,
+      path: data.req.path,
+      xhr: data.req.xhr
+    }
+  });
+};
+
+const errorLogger = createLogger({
+  format: format.json(),
+  transports: [
+    new transports.Console(),
+    new transports.MongoDB({
+      level: "error",
+      db: process.env.MONGODB_URI,
+      collection: "errors"
+    })
+  ]
+});
+
+export const logError = async message => {
+  const result = await errorLogger.error(message, {
+    metadata: {
+      time: new Date()
+    }
+  });
 };
