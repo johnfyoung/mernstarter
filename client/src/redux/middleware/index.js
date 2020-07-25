a8245f9b01a2a33cfad737dee454fe71601396ae
John Young <john@codeandcreative.com>
2020-07-24 18:47:37 -0700

    Makes the app testable

--- client/src/redux/middleware/index.js
+++ client/src/redux/middleware/index.js
@@ -2,9 +2,16 @@ import { applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { logger, userEvent } from "./logger";
 
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (
+  process.env.NODE_ENV === "prod" ||
+  process.env.NODE_ENV === "production" ||
+  process.env.TESTING
+) {
+  devTools = (a) => a;
+}
+
 // Redux Thunk middleware allows you to write action creators that return a function instead of an action
 
-export default compose(
-  applyMiddleware(thunk, logger, userEvent),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export default compose(applyMiddleware(thunk, logger, userEvent), devTools);
--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,9 +1,64 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+import configureStore from "redux-mock-store";
 import App from "../components/App";
+require("dotenv").config();
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const mockStore = configureStore([]);
+
+describe("App Component", () => {
+  let store = null;
+  let component = null;
+
+  beforeEach(() => {
+    store = mockStore({
+      alert: { alert: null, announcement: null },
+      auth: {},
+      nav: {
+        brand: {
+          path: "/",
+          label: process.env.REACT_APP_NAME,
+        },
+        hasSearch: false,
+        menu: {
+          home: {
+            path: "/",
+            label: "Home",
+            privilege: false,
+          },
+          admin: {
+            path: "/admin",
+            label: "Admin",
+            privilege: true,
+          },
+        },
+        submenu: {
+          profile: {
+            path: "/profile",
+            label: "Profile",
+            privilege: true,
+          },
+        },
+      },
+      service: {
+        latlong: null,
+        error: {},
+      },
+    });
+
+    component = renderer.create(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+
+  it("renders without crashing", () => {
+    // const div = document.createElement("div");
+    // ReactDOM.render(<App />, div);
+    // ReactDOM.unmountComponentAtNode(div);
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });
