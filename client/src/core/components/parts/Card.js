8ed1bd19b770ab6010760e1a25179feae7130890
John Young <john@codeandcreative.com>
2020-08-07 14:40:48 -0700

    Adds a card component

---
+++ client/src/core/components/parts/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Card({ head, title, children, cta, url, md, lg }) {
+  return (
+    <div className={`col-md-${md ? md : 12} col-lg-${lg ? lg : 12}`}>
+      <div className={`card`}>
+        {head && <div className="card-header">{head}</div>}
+        <div className="card-body">
+          {title && <h5 className="card-title">{title}</h5>}
+          <p className="card-text">{children}</p>
+          {cta && (
+            <a href={url} className="btn btn-primary">
+              {cta}
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
